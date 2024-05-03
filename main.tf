1946880ca9ffe0c0d06e842ebd086b324f773467
Khang Nguyen <affaxerdcheruiyot@gmail.com>
2024-05-04 00:22:13 +0300

    ft: security groups

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ resource "aws_instance" "example" {
     ami     = "ami-053a617c6207ecc7b"
     instance_type = "t2.micro"
 
+    user_data = <<-EOF
+                #!bin/bash
+                echo "Hello world" > index.html
+                nohup busybox httpf -f -p 8080 &
+                EOF
+    user_data_replace_on_change = true
+
     tags = {
         Name    =   "Terraform_Example"
     }
